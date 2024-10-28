7138d2d9a2efc8314a742e7b7beb0b39bc65a358
hogyzen12 <billypapas12@gmail.com>
2024-10-28 22:45:04 +0100

    patching wallet name to make runnign ezpz

--- calypso/calypso.js
+++ calypso/calypso.js
@@ -25,7 +25,7 @@ const TOKEN_IDS = {
 };
 
 const RPC_ENDPOINT = process.env.RPC_ENDPOINT || "https://mainnet.helius-rpc.com/?api-key=2c0388dc-a082-4cc5-bad9-29437f3c0715";
-const KEYPAIR_PATH = process.env.KEYPAIR_PATH || "4Cbjzus4yN733FaSDsjC63QmymwWztPCHHcJ7nVeaf2z.json";
+const KEYPAIR_PATH = process.env.KEYPAIR_PATH || "calypso.json";
 const REBALANCE_THRESHOLD = new Decimal(process.env.REBALANCE_THRESHOLD || '0.0042');
 const CHECK_INTERVAL = parseInt(process.env.CHECK_INTERVAL, 10) || 60;
 
